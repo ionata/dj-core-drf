3f812c755dcef288a9f65cc5499fe60bb6137847
Chris Darko <chris@ionata.com.au>
2017-07-18 09:58:01 +1000

    Make setup.py use markdown

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import io
 from setuptools import setup, find_packages
 
 
-with io.open('README.rst', encoding='utf-8') as handle:
+with io.open('README.md', encoding='utf-8') as handle:
     readme = handle.read()
 
 
