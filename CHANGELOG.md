a7a9d2cfc22b932839f34c82399b7e823ea47d8d
Chris Darko <chris@ionata.com.au>
2017-09-19 13:48:00 +1000

    Update for dj-core 0.0.6

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,5 @@
 # DEV
+- Update for dj-core 0.0.6
 - Move package to top-level
 # 0.0.4
 - Update dependencies
--- dj_core_drf/config.py
+++ dj_core_drf/config.py
@@ -1,28 +1,29 @@
 from datetime import timedelta
 
-from dj_core.config import Config as BaseConfig
+from dj_core.config import Config as BaseConfig, DefaultProxy
 
 
 class Config(BaseConfig):
-    defaults_dict = BaseConfig.defaults_dict.copy()
-    defaults_dict.simple.update({
-        'DJCORE_ACCOUNT_AUTHENTICATION_METHOD': 'email',
-        'DJCORE_ACCOUNT_EMAIL_REQUIRED': True,
-        'DJCORE_ACCOUNT_EMAIL_VERIFICATION': 'none',
-        'DJCORE_ACCOUNT_USER_MODEL_USERNAME_FIELD': None,
-        'DJCORE_ACCOUNT_USERNAME_REQUIRED': False,
-        'DJCORE_ACCOUNT_REGISTRATION': 'enabled',
-        'DJCORE_REST_USE_JWT': True,
-        'DJCORE_SWAGGER_SETTINGS': {'api_version': '1'},
-        'DJCORE_JWT_AUTH': {
+    defaults = BaseConfig.defaults.copy()
+    defaults.update({
+        'ACCOUNT_AUTHENTICATION_METHOD': 'email',
+        'ACCOUNT_EMAIL_REQUIRED': True,
+        'ACCOUNT_EMAIL_VERIFICATION': 'none',
+        'ACCOUNT_USER_MODEL_USERNAME_FIELD': None,
+        'ACCOUNT_USERNAME_REQUIRED': False,
+        'ACCOUNT_REGISTRATION': 'enabled',
+        'REST_USE_JWT': True,
+        'SWAGGER_SETTINGS': {'api_version': '1'},
+        'JWT_AUTH': {
             'JWT_EXPIRATION_DELTA': timedelta(hours=24),
             'JWT_AUTH_HEADER_PREFIX': 'Token',
-        }
+        },
+        'REST_FRAMEWORK': DefaultProxy({}, 'get_drf_settings'),
+        'REST_AUTH_SERIALIZERS': DefaultProxy({}, 'get_rest_auth_serializers'),
     })
 
     def get_installed_apps(self, settings):
         return super(Config, self).get_installed_apps(settings) + [
-            'dj_core_drf',
             'rest_framework',
             'rest_framework.authtoken',
             'rest_framework_swagger',
@@ -57,9 +58,3 @@ class Config(BaseConfig):
             'PASSWORD_RESET_SERIALIZER': 'dj_core_drf.serializers.PasswordResetSerializer',
             'LOGIN_SERIALIZER': 'dj_core_drf.serializers.LoginSerializer',
         }
-
-    def get_settings(self):
-        settings = super(Config, self).get_settings()
-        settings.REST_FRAMEWORK = self.get_drf_settings(settings)
-        settings.REST_AUTH_SERIALIZERS = self.get_rest_auth_serializers(settings)
-        return settings
